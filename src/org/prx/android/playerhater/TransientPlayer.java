7781b57cad96a67412a5a1ff09ab4b575a7d98f7
chrisrhoden <carhoden@gmail.com>
2012-02-06 23:41:05 -0500

    I guess I didnt understand the way that requesting focus would work. this should be better

--- src/org/prx/android/playerhater/TransientPlayer.java
+++ src/org/prx/android/playerhater/TransientPlayer.java
@@ -4,9 +4,12 @@ import java.io.FileDescriptor;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.util.Log;
 import android.media.MediaPlayer.OnCompletionListener;
 
 public class TransientPlayer {
+
+	private static final String TAG = "TransientPlayer";
 
 	public static void play(Context c, String url, boolean isDuckable) {
 		new TransientPlayer(c, url, isDuckable).play();
@@ -42,13 +45,13 @@ public class TransientPlayer {
 		this.isDuckable = isDuckable;
 		this.playType = FILE;
 	}
-	
+
 	private void play() {
 		TransientPlayerTask task = new TransientPlayerTask();
 		Thread thread = new Thread(task);
 		thread.run();
 	}
-	
+
 	private int getDurationHint() {
 		if (isDuckable) {
 			return AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK;
@@ -56,14 +59,14 @@ public class TransientPlayer {
 			return AudioManager.AUDIOFOCUS_GAIN_TRANSIENT;
 		}
 	}
-	
+
 	private class TransientPlayerTask implements Runnable {
-		
+
 		@Override
 		public void run() {
-			
+
 			wrapper.setAudioStreamType(AudioManager.STREAM_NOTIFICATION);
-			
+
 			try {
 				switch (playType) {
 				case FILE:
@@ -78,19 +81,19 @@ public class TransientPlayer {
 				e.printStackTrace();
 				return;
 			}
-			
+
 			final AudioManager audioManager = (AudioManager)c.getSystemService(Context.AUDIO_SERVICE);
-			
+
 			final AudioManager.OnAudioFocusChangeListener audioFocusListener = new AudioManager.OnAudioFocusChangeListener() {
 				@Override
 				public void onAudioFocusChange(int focusChange) {
-					if (focusChange == getDurationHint())
-						wrapper.start();
+					// I don't think we need to do anything here.
+					// generally, we might want to stop the audio in
+					// the event that we have lost focus in a non-
+					// transient way, I think.
 				}
 			};
-			
-			audioManager.requestAudioFocus(audioFocusListener, AudioManager.STREAM_NOTIFICATION, getDurationHint());
-			
+
 			wrapper.setOnCompletionListener(new OnCompletionListener() {
 
 				@Override
@@ -98,9 +101,14 @@ public class TransientPlayer {
 					audioManager.abandonAudioFocus(audioFocusListener);
 					wrapper.release();
 				}
-				
+
 			});
+
+			int status = audioManager.requestAudioFocus(audioFocusListener, AudioManager.STREAM_NOTIFICATION, getDurationHint());
+
+			if (status == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+				wrapper.start();
 		}
-		
+
 	}
 }
