507230ea2070740a9a3528c29e1c2dadf83e1d25
chrisrhoden <carhoden@gmail.com>
2013-06-14 13:56:16 -0400

    copyright

--- src/main/java/org/prx/playerhater/mediaplayer/StatelyPlayer.java
+++ src/main/java/org/prx/playerhater/mediaplayer/StatelyPlayer.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-
 package org.prx.playerhater.mediaplayer;
 
 import java.io.IOException;
